ae74593a77f622505e088e1f9923943b9d3a1b00
Colby Aley <colby@aley.me>
2014-01-11 11:49:49 -0800

    Adding db connect back

--- environment.rb
+++ environment.rb
@@ -7,6 +7,9 @@ require 'aws/s3'
 require 'json'
 require 'securerandom'
 
+# Establish database connection
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/commitphotos')
+
 require File.expand_path('../lib/s3', __FILE__)
 
 Dir.glob('./models/*.rb').each{ |m| require m }
